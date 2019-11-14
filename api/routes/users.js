8099b38b77b65f2eebded1abd3a13a20d98842e7
Janne Mattila <janne.mattila@hotmail.fi>
2019-11-14 15:13:08 +0200

    Add vs code configs

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,7 +6,7 @@ const dbAddress = process.env.DB_ADDRESS || "http://localhost:3002";
 
 router.get('/', function(req, res, next) {
   request(dbAddress + '/api/users', function(err, response, body) {
-    if (err) {
+    if (!err) {
       res.json([]);
       return;
     }
